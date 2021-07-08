5ca118c13a3a52f125f9d484f303f8198a4c0524
Darshan Subhash Khope <64018424+kdarshan425@users.noreply.github.com>
2021-07-08 20:07:56 +0530

    updated header

--- partials/_header.php
+++ partials/_header.php
@@ -2,41 +2,52 @@
 session_start();
 
 echo'
+
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
+  
     <a class="navbar-brand" href="https://dkforum.herokuapp.com/">DKForum</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                
-        <li></li>
-       
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">                
+        <li></li>       
       </ul>';
+
       if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true){
         echo'
-        <form class="d-flex" style="padding-right: 10px;" action="search.php" method="get">
-        <input class="form-control me-2" type="search" name="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-       <a href="partials/_logout.php" role="button" class="btn btn-outline-success" type="submit">LOGOUT</a>';
+        <form class="d-flex" style="padding: 10px;" action="search.php" method="get">
+          <input class="form-control me-2" type="search" name="search" placeholder="Search threads" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        
+        <div style="padding:10px;">
+        <a href="partials/_logout.php" role="button" class="btn btn-outline-success" type="submit">LOGOUT</a>
+        </div>';
       }
       else{
         echo'
-        <form class="d-flex" style="padding-right: 10px;" action="search.php" method="get">
-        <input class="form-control me-2" type="search" name="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-      <button style="padding-right: 10px;" class="btn btn-outline-success" type="submit"  data-bs-toggle="modal" data-bs-target="#loginModal">login</button>
-      <button style="padding-right: 10px;" class="btn btn-outline-success" type="submit" data-bs-toggle="modal" data-bs-target="#signupModal">Signup</button>';
+        <div style="padding: 10px;">
+          <button  class="btn " type="submit"  data-bs-toggle="modal" data-bs-target="#loginModal">login</button>
+        </div>
+
+        <div style="padding: 10px;">
+        <button  class="btn " type="submit" data-bs-toggle="modal" data-bs-target="#signupModal">Signup</button>
+        </div>
+
+        <form class="d-flex" style="padding: 10px;" action="search.php" method="get">
+          <input class="form-control me-2" type="search" name="search" placeholder="Search threads" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>';
       }
       
 
        echo '</div>
   </div>
 </nav>';
-
 
 $id = isset($_GET['$showError']);
  include 'partials/_loginModal.php';
