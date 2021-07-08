7df96e0a7a0a8b802482a38fc0fce29015298d5a
Darshan Subhash Khope <64018424+kdarshan425@users.noreply.github.com>
2021-07-08 19:26:51 +0530

    header updated

--- partials/_header.php
+++ partials/_header.php
@@ -4,7 +4,7 @@ session_start();
 echo'
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/dk/">DKForum</a>
+    <a class="navbar-brand" href="https://dkforum.herokuapp.com/">DKForum</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
