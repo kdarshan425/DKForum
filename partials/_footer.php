051e042604c0dd49eeb9b4113a22ff81a200cd27
Darshan Subhash Khope <64018424+kdarshan425@users.noreply.github.com>
2021-07-09 11:33:17 +0530

    updated

--- partials/_footer.php
+++ partials/_footer.php
@@ -2,7 +2,7 @@
 echo'
 <footer style="width:100vh;background: black;padding: 24px; width: 100%">
     <center>
-        <p style="color: white;font-weight: 300;"> Powered by Darshan Khope<br>Kindly mail at kdarshan425@gmail.com<br>© 2020 COPYRIGHT</p>
+        <p style="color: white;font-weight: 300;"> Powered by <a href="https://kdarshan425.github.io/darshankhope/">Darshan Khope</a><br>© 2020 COPYRIGHT</p>
     </center>            
 </footer>
 ';
