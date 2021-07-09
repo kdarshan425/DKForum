60a14596d0c4ad5d4a1fd22ac75f2200a9b1d10a
Darshan Subhash Khope <64018424+kdarshan425@users.noreply.github.com>
2021-07-09 11:21:44 +0530

    updated
    Updated

--- partials/_signupModal.php
+++ partials/_signupModal.php
@@ -13,16 +13,16 @@
                 <form action="partials\_handleSignup.php" method="post">
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" name="signupEmail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <input type="email" name="signupEmail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" name="signupPassword" class="form-control" id="exampleInputPassword1">
+                        <input type="password" name="signupPassword" class="form-control" id="exampleInputPassword1" required>
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
-                        <input type="password" name="signupcPassword" class="form-control" id="exampleInputPassword1">
+                        <input type="password" name="signupcPassword" class="form-control" id="exampleInputPassword1" required>
                     </div>
                     
                     <button type="submit" class="btn btn-primary">Submit</button>
